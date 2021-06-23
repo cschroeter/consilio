b7cce110bdd8e801deb4fd6c1420913ff41b0ad8
Christian Schröter <christian.schroeter@logmein.com>
2021-06-23 11:08:35 +0200

    chore: bump deps

--- packages/library/src/Button/Button.tsx
+++ packages/library/src/Button/Button.tsx
@@ -6,12 +6,17 @@ interface Props {
 
 export const Button: React.FC<Props> = (props) => {
   const handleClick = () => {
-    debugger
     console.log('clicked')
   }
   return (
     <button
-      style={{ backgroundColor: 'red', padding: '0.5rem 1rem', border: 0, fontSize: '1rem' }}
+      style={{
+        color: 'white',
+        backgroundColor: 'blue',
+        padding: '0.5rem 1rem',
+        border: 0,
+        fontSize: '1rem',
+      }}
       onClick={handleClick}
     >
       {props.children}
